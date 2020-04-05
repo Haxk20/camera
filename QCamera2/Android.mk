Evervolv/android_hardware_qcom_camera

Do not use perfd by default
Add persist.camera.perfd.enable to disable/enable perfd connection from
CameraHAL.

Bug: 63589458
Test: Test powerHAL hint working with camera and there is no perfd interaction
Change-Id: Ic288a494be995f24cc85ceb69539aeb0a2d174c3

msm8998/QCamera2/Android.mk
@@ -138,7 +138,7 @@ LOCAL_C_INCLUDES += \
 LOCAL_SHARED_LIBRARIES := liblog libhardware libutils libcutils libdl libsync
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
-LOCAL_SHARED_LIBRARIES += libcutils libdl libhdrplusclient
+LOCAL_SHARED_LIBRARIES += libbase libcutils libdl libhdrplusclient
 LOCAL_SHARED_LIBRARIES += libhidlbase libhwbinder libutils android.hardware.power@1.0
 ifeq ($(TARGET_TS_MAKEUP),true)
 LOCAL_SHARED_LIBRARIES += libts_face_beautify_hal libts_detected_face_hal

Haxk20/camera

QCamera2/Android.mk
@@ -176,7 +176,7 @@ LOCAL_C_INCLUDES += \
 LOCAL_SHARED_LIBRARIES := liblog libhardware libutils libcutils libdl libsync
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
-LOCAL_SHARED_LIBRARIES += libcutils libdl libhal_dbg
+LOCAL_SHARED_LIBRARIES += libbase libcutils libdl libhal_dbg
 ifneq ($(TARGET_KERNEL_VERSION),$(filter $(TARGET_KERNEL_VERSION),3.18 4.4 4.9))
 LOCAL_SHARED_LIBRARIES += libion
 LOCAL_CFLAGS += -DVIDEO_EXPLICIT_UBWC